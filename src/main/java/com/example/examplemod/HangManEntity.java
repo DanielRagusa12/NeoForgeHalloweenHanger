c1aba3d4d217b87aa5a6e7cd0735cda3fe542504
DanielRagusa12 <daniel.ragusa129@gmail.com>
2024-10-02 00:42:48 -0500

    _

--- src/main/java/com/example/examplemod/HangManEntity.java
+++ src/main/java/com/example/examplemod/HangManEntity.java
@@ -13,15 +13,15 @@ import com.example.examplemod.HangManCommon;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 
-@EventBusSubscriber(modid = HangManCommon.MODID, bus = EventBusSubscriber.Bus.MOD)
+//@EventBusSubscriber(modid = HangManCommon.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class HangManEntity {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(HangManCommon.MODID);
     public static final DeferredItem<Item> ENTITY_TYPES = ITEMS.registerSimpleItem(HangManCommon.MODID);
-    public static final DeferredRegister<EntityType<?>> HANG_DUMMY = ENTITY_TYPES.register(HangManCommon.noose,
-            () -> EntityType.Builder.of(ForgeNooseEntity::new, MobCategory.MISC)
-                    .setTrackingRange(256).setUpdateInterval(20).noSummon().sized(0.001F, 0.001F).build(HangManCommon.noose));
+    //public static final DeferredRegister<EntityType<?>> HANG_DUMMY = ENTITY_TYPES.register(HangManCommon.noose,
+            //() -> EntityType.Builder.of(ForgeNooseEntity::new, MobCategory.MISC)
+                    //.setTrackingRange(256).setUpdateInterval(20).noSummon().sized(0.001F, 0.001F).build(HangManCommon.noose));
 
-    public static final DeferredRegister<EntityType<CameraPlayerOnNoose>> CAMERA = ENTITY_TYPES.register(HangManCommon.camera,
-            () -> EntityType.Builder.<CameraPlayerOnNoose>of(CameraPlayerOnNoose::new, MobCategory.MISC)
-                    .setTrackingRange(256).setUpdateInterval(20).noSave().noSummon().sized(0.001F, 0.001F).build(HangManCommon.camera));
+    //public static final DeferredRegister<EntityType<CameraPlayerOnNoose>> CAMERA = ENTITY_TYPES.register(HangManCommon.camera,
+           // () -> EntityType.Builder.<CameraPlayerOnNoose>of(CameraPlayerOnNoose::new, MobCategory.MISC)
+                 //   .setTrackingRange(256).setUpdateInterval(20).noSave().noSummon().sized(0.001F, 0.001F).build(HangManCommon.camera));
 }
